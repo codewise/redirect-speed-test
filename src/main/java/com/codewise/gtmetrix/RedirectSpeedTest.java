36793b59b51239f1f3bed5b91d7f7986d0078e1a
Jacek Psiuk <jacek.psiuk@codewise.com>
2021-09-03 12:21:06 +0200

    Add some improvements

--- src/main/java/com/codewise/gtmetrix/RedirectSpeedTest.java
+++ src/main/java/com/codewise/gtmetrix/RedirectSpeedTest.java
@@ -4,6 +4,7 @@ import com.csvreader.CsvWriter;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class RedirectSpeedTest {
@@ -15,17 +16,27 @@ public class RedirectSpeedTest {
     private static final String LANDING_PAGE_DOMAIN = "google";
 
     private static final int ATTEMPTS_PER_LOCATION = 10; // Number of tries per location, for each of the locations
-    private static final int MAX_CONCURRENT_TESTS = 8; // 8 for PRO account, 2 for Free
+    private static final int MAX_CONCURRENT_TESTS = 2; // 8 for PRO account, 2 for Free
     private static final int BROWSER_CODE = 3; // 3 for Chrome, 1 for Firefox
+
+    private static final Map<Integer, String> LOCATIONS = Map.of(
+            1, "Vancouver, Canada",
+            2, "London, UK",
+            3, "Sydney, Australia",
+            4, "San Antonio, TX, USA",
+            5, "Mumbai, India",
+            6, "São Paulo, Brazil",
+            7, "Hong Kong, China"
+    );
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Gtmetrix gtmetrix = new Gtmetrix(EMAIL, API_KEY);
         CsvWriter csvWriter = new CsvWriter("report.csv");
-        csvWriter.writeRecord(new String[] {"url", "location", "attempt", "time"});
+        writeToCsv(csvWriter, "url", "id", "location", "attempt", "time");
 
         LinkedList<ScheduledTest> scheduledTests = new LinkedList<>();
         // Check report for every location
-        for (int location = 1; location <= 7; location++) {
+        for (int location : LOCATIONS.keySet()) {
             int attempts = 0;
             int processedReports = 0;
 
@@ -38,45 +49,47 @@ public class RedirectSpeedTest {
                     scheduledTests.add(new ScheduledTest(
                             gtmetrix.scheduleTest(location, 3, URL), location, BROWSER_CODE, URL, ++attempts)
                     );
-                    System.out.println(String.format("Scheduled a test: %s", scheduledTests.peekLast().getTestId()));
+                    System.out.printf("Scheduled a test: %s%n", scheduledTests.peekLast().getTestId());
 
                     // If there's currently no report to be scheduled, check if the oldest one is ready, and if it is
                     // Try to process it. If it's not ready wait a second and try again (as a simple precaution
                     // not to fire too many requests at GTmetrix
                 } else {
+                    ScheduledTest test = scheduledTests.getFirst();
+                    String testId = test.getTestId();
                     try {
-                        System.out.println(String.format(
-                                "Can't schedule any more tests, checking if the oldest scheduled is completed...(%s)",
-                                scheduledTests.peekFirst().getTestId())
-                        );
+                        System.out.printf("Checking if the oldest scheduled test is completed...(%s)%n", testId);
                         // If there's a completed test avaiable to analyze do it, if not wait a second and try again
-                        if (gtmetrix.isTestCompleted(scheduledTests.peekFirst().getTestId())) {
-                            // If test is completed, poll it from the list
-                            ScheduledTest currentTest = scheduledTests.pollFirst();
-                            long timeUntilLander = gtmetrix.getTimeUntilLanderFromHar(currentTest.getTestId(),
-                                    LANDING_PAGE_DOMAIN);
+                        if (gtmetrix.isTestCompleted(testId)) {
+                            scheduledTests.removeFirst();
                             processedReports++;
-                            System.out.println(String.format("Id: %s, location: %s, url: %s, time: %s",
-                                    currentTest.getTestId(), location, URL, timeUntilLander));
-                            csvWriter.writeRecord(new String[] {URL, String.valueOf(location),
-                                    String.valueOf(currentTest.getAttemptNumber()), String.valueOf(timeUntilLander)});
+                            long timeUntilLander = gtmetrix.getTimeUntilLanderFromHar(testId, LANDING_PAGE_DOMAIN);
+                            System.out.printf("Id: %s, location: %s, url: %s, time: %s%n",
+                                    testId, location, URL, timeUntilLander);
+                            writeToCsv(csvWriter, URL, testId,
+                                    LOCATIONS.get(location), String.valueOf(test.getAttemptNumber()),
+                                    String.valueOf(timeUntilLander));
                             csvWriter.flush();
                         } else {
-                            TimeUnit.SECONDS.sleep(1);
                             System.out.println("It's not, trying again in a second...");
+                            TimeUnit.SECONDS.sleep(1);
                         }
                     } catch (IllegalStateException e) {
-                        // If there's an error thrown (502 or SSL issues) write it down simply as an 'error'
-                        ScheduledTest currentTest = scheduledTests.pollFirst();
-                        System.out.println(String.format("Id: %s, location: %s, url: %s, time: %s",
-                                currentTest.getTestId(), location, URL, "error"));
-                        csvWriter.writeRecord(new String[] {URL, String.valueOf(location),
-                                String.valueOf(currentTest.getAttemptNumber()), "error"});
+                        scheduledTests.removeFirst();
+                        processedReports++;
+                        System.out.printf("Id: %s, location: %s, url: %s, time: %s%n",
+                                testId, location, URL, "error: " + e);
+                        writeToCsv(csvWriter, URL, testId, LOCATIONS.get(location),
+                                String.valueOf(test.getAttemptNumber()), "error: " + e);
                         csvWriter.flush();
                     }
                 }
             }
         }
     }
-}
 
+    private static void writeToCsv(CsvWriter csvWriter, String url, String testId, String location, String attempt,
+            String result) throws IOException {
+        csvWriter.writeRecord(new String[] {url, testId, location, attempt, result});
+    }
+}
