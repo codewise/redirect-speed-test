0589565e06f1830d99dd8f5402f9000223bfae57
Andrzej Piechowicz <andrzej.piechowicz@codewise.com>
2023-04-17 14:55:15 +0200

    Extract and refactor test running logic into TestRunner

--- src/main/java/com/codewise/gtmetrix/RedirectSpeedTest.java
+++ src/main/java/com/codewise/gtmetrix/RedirectSpeedTest.java
@@ -3,102 +3,17 @@ package com.codewise.gtmetrix;
 import com.codewise.gtmetrix.configuration.Configuration;
 import com.codewise.gtmetrix.configuration.ConfigurationReader;
 import com.codewise.gtmetrix.output.OutputHandler;
-import com.csvreader.CsvWriter;
-
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import com.codewise.gtmetrix.test.TestRunner;
 
 public class RedirectSpeedTest {
 
     private static final String CONFIGURATION_FILE = "configuration.yml";
     private static final String OUTPUT_FILE = "results.csv";
 
-    // Put your values here
-    private static final String EMAIL = "email";
-    private static final String API_KEY = " apiKey";
-    private static final String URL = "https://url_to_test";
-    private static final String LANDING_PAGE_DOMAIN = "google";
-
-    private static final int ATTEMPTS_PER_LOCATION = 10; // Number of tries per location, for each of the locations
-    private static final int MAX_CONCURRENT_TESTS = 2; // 8 for PRO account, 2 for Free
-    private static final int BROWSER_CODE = 3; // 3 for Chrome, 1 for Firefox
-
-    private static final Map<Integer, String> LOCATIONS = Map.of(
-            1, "Vancouver, Canada",
-            2, "London, UK",
-            3, "Sydney, Australia",
-            4, "San Antonio, TX, USA",
-            5, "Mumbai, India",
-            6, "São Paulo, Brazil",
-            7, "Hong Kong, China"
-    );
-
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         Configuration configuration = new ConfigurationReader().readConfiguration(CONFIGURATION_FILE);
         try (OutputHandler outputHandler = OutputHandler.getHandlerForFile(OUTPUT_FILE)) {
+            new TestRunner(configuration, outputHandler).runTests();
         }
-        Gtmetrix gtmetrix = new Gtmetrix(EMAIL, API_KEY);
-        CsvWriter csvWriter = new CsvWriter("report.csv");
-        writeToCsv(csvWriter, "url", "id", "location", "attempt", "time");
-
-        LinkedList<ScheduledTest> scheduledTests = new LinkedList<>();
-        // Check report for every location
-        for (int location : LOCATIONS.keySet()) {
-            int attempts = 0;
-            int processedReports = 0;
-
-            // Stop loop for location only if processed all reports from it
-            while (processedReports < ATTEMPTS_PER_LOCATION) {
-
-                // Schedule a report to be processed only if we've not reached cap on reports 'in progress' yet, and
-                // if we haven't scheduled all that we want to (attemptsPerLocation)
-                if (scheduledTests.size() < MAX_CONCURRENT_TESTS && attempts < ATTEMPTS_PER_LOCATION) { //
-                    scheduledTests.add(new ScheduledTest(
-                            gtmetrix.scheduleTest(location, 3, URL), location, BROWSER_CODE, URL, ++attempts)
-                    );
-                    System.out.printf("Scheduled a test: %s%n", scheduledTests.peekLast().getTestId());
-
-                    // If there's currently no report to be scheduled, check if the oldest one is ready, and if it is
-                    // Try to process it. If it's not ready wait a second and try again (as a simple precaution
-                    // not to fire too many requests at GTmetrix
-                } else {
-                    ScheduledTest test = scheduledTests.getFirst();
-                    String testId = test.getTestId();
-                    try {
-                        System.out.printf("Checking if the oldest scheduled test is completed...(%s)%n", testId);
-                        // If there's a completed test avaiable to analyze do it, if not wait a second and try again
-                        if (gtmetrix.isTestCompleted(testId)) {
-                            scheduledTests.removeFirst();
-                            processedReports++;
-                            long timeUntilLander = gtmetrix.getTimeUntilLanderFromHar(testId, LANDING_PAGE_DOMAIN);
-                            System.out.printf("Id: %s, location: %s, url: %s, time: %s%n",
-                                    testId, location, URL, timeUntilLander);
-                            writeToCsv(csvWriter, URL, testId,
-                                    LOCATIONS.get(location), String.valueOf(test.getAttemptNumber()),
-                                    String.valueOf(timeUntilLander));
-                            csvWriter.flush();
-                        } else {
-                            System.out.println("It's not, trying again in a second...");
-                            TimeUnit.SECONDS.sleep(1);
-                        }
-                    } catch (IllegalStateException e) {
-                        scheduledTests.removeFirst();
-                        processedReports++;
-                        System.out.printf("Id: %s, location: %s, url: %s, time: %s%n",
-                                testId, location, URL, "error: " + e);
-                        writeToCsv(csvWriter, URL, testId, LOCATIONS.get(location),
-                                String.valueOf(test.getAttemptNumber()), "error: " + e);
-                        csvWriter.flush();
-                    }
-                }
-            }
-        }
-    }
-
-    private static void writeToCsv(CsvWriter csvWriter, String url, String testId, String location, String attempt,
-            String result) throws IOException {
-        csvWriter.writeRecord(new String[] {url, testId, location, attempt, result});
     }
 }
---
+++ src/main/java/com/codewise/gtmetrix/test/TestRunner.java
@@ -0,0 +1,147 @@
+package com.codewise.gtmetrix.test;
+
+import com.codewise.gtmetrix.api.GtmetrixApi;
+import com.codewise.gtmetrix.configuration.Browser;
+import com.codewise.gtmetrix.configuration.Configuration;
+import com.codewise.gtmetrix.configuration.Location;
+import com.codewise.gtmetrix.output.OutputHandler;
+import com.codewise.gtmetrix.test.data.ScheduledTest;
+import com.codewise.gtmetrix.test.data.TestResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class TestRunner {
+
+    private static final Logger log = LogManager.getLogger(TestRunner.class);
+    private static final Duration SLEEP_TIME = Duration.ofSeconds(10);
+    private final Configuration configuration;
+    private final OutputHandler outputHandler;
+    private final GtmetrixApi gtmetrixApi;
+    private final Set<ScheduledTest> scheduledTests;
+
+    public TestRunner(Configuration configuration, OutputHandler outputHandler) {
+        this.configuration = configuration;
+        this.outputHandler = outputHandler;
+        gtmetrixApi = new GtmetrixApi(configuration.getApiKey());
+        scheduledTests = new LinkedHashSet<>();
+    }
+
+    public void runTests() {
+        checkIfTestsCanBeRun();
+        doRunTests();
+        waitForTestResults();
+    }
+
+    private void checkIfTestsCanBeRun() {
+        double expectedApiUsage = calculateExpectedApiUsage();
+        try {
+            double availableApiCredits = gtmetrixApi.checkAvailableApiCredits();
+            if (expectedApiUsage > availableApiCredits) {
+                String message = "Available credits (%s) not enough to execute all tests (total credits required: %s)"
+                        .formatted(availableApiCredits, expectedApiUsage);
+                log.error(message);
+                throw new InsufficientApiCreditsException(message);
+            }
+            log.debug("Tests can be run (credits that will be used: {}, currently available credits: {})",
+                    expectedApiUsage, availableApiCredits);
+        } catch (IOException e) {
+            log.error("Unable to check available api credits", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private double calculateExpectedApiUsage() {
+        int numberOfRequestsToBeMade = configuration.getLocations().size() * configuration.getBrowsers().size()
+                * configuration.getNumberOfTestsToRunForBrowserAndLocation();
+        return numberOfRequestsToBeMade * configuration.getReportType().getCreditsUse();
+    }
+
+    private void doRunTests() {
+        for (Location location : configuration.getLocations()) {
+            for (Browser browser : configuration.getBrowsers()) {
+                int numberOfTestsToRun = configuration.getNumberOfTestsToRunForBrowserAndLocation();
+                for (int testNumber = 1; testNumber <= numberOfTestsToRun; testNumber++) {
+                    runTest(location, browser, testNumber);
+                }
+            }
+        }
+    }
+
+    private void runTest(Location location, Browser browser, int testNumber) {
+        if (isMaxConcurrentRequestsLimitMet()) {
+            waitForRequestSlot();
+        }
+        scheduleTest(location, browser, testNumber);
+    }
+
+    private boolean isMaxConcurrentRequestsLimitMet() {
+        return scheduledTests.size() >= configuration.getNumberOfConcurrentTests();
+    }
+
+    private void waitForRequestSlot() {
+        while (isMaxConcurrentRequestsLimitMet()) {
+            checkAndHandleCompletedTests();
+            if (isMaxConcurrentRequestsLimitMet()) {
+                waitWithoutThrowing();
+            }
+        }
+    }
+
+    private void checkAndHandleCompletedTests() {
+        scheduledTests.stream()
+                .map(this::getTestResult)
+                .flatMap(Optional::stream)
+                .peek(outputHandler::saveTestResult)
+                .map(TestResult::toScheduledTest)
+                .toList()
+                .forEach(scheduledTests::remove);
+    }
+
+    private Optional<TestResult> getTestResult(ScheduledTest scheduledTest) {
+        String testId = scheduledTest.testId();
+        try {
+            return gtmetrixApi.getTestResult(scheduledTest);
+        } catch (IOException e) {
+            log.error("Failed to check state for test: {}", testId, e);
+            return Optional.empty();
+        }
+    }
+
+    private void waitWithoutThrowing() {
+        try {
+            log.debug("Going to sleep for {} seconds", SLEEP_TIME.toSeconds());
+            Thread.sleep(SLEEP_TIME.toMillis());
+            log.debug("Woke up");
+        } catch (InterruptedException e) {
+            log.warn("Error while waiting", e);
+        }
+    }
+
+    private void scheduleTest(Location location, Browser browser, int testNumber) {
+        String url = configuration.getUrlToTest();
+        String reportType = configuration.getReportType().getName();
+        try {
+            ScheduledTest scheduledTest = gtmetrixApi.scheduleTest(url, location, browser, reportType, testNumber);
+            scheduledTests.add(scheduledTest);
+            log.info("Scheduled test: {}", scheduledTest.testId());
+        } catch (IOException e) {
+            log.error("Failed to schedule test for location: {}, browser: {}, attemptNumber: {}", location, browser,
+                    testNumber, e);
+        }
+    }
+
+    private void waitForTestResults() {
+        while (!scheduledTests.isEmpty()) {
+            checkAndHandleCompletedTests();
+            if (!scheduledTests.isEmpty()) {
+                waitWithoutThrowing();
+            }
+        }
+    }
+}
